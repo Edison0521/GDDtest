3ebeb7963f95bc5c8bfe8c334b3f1df657034e2d
Zhang <zhangvan605@gmail.com>
2023-02-20 13:05:24 +0800

    v220.noon

--- GDDMining/GDDmining.py
+++ GDDMining/GDDmining.py
@@ -102,7 +102,7 @@ def relationbolck(item,seg):
                     val = DDsimilars(cleanlist[i],cleanlist[j])
                     valuelist.append(val)
         tt = calculate_delta(valuelist, con)
-        print("The best delta of " + str(item) + " is " + str(abs(tt)))
+        #print("The best delta of " + str(item) + " is " + str(abs(tt)))
         distance = abs(tt)
         distancelist = splitdistance(distance, seg)
         nxms = []
@@ -125,7 +125,7 @@ def relationbolck(item,seg):
     else:
         d = df[item].tolist()
         cleanlist = list(set(d))
-        print("The Distance of " + str(item) + " is " + str(0))
+        #print("The Distance of " + str(item) + " is " + str(0))
         distance = 0
         distancelist = splitdistance(distance, seg)
         nxms = []
@@ -141,6 +141,86 @@ def relationbolck(item,seg):
                     m = GDD.iteml(item + '=' + str(val), str(dis), l2, str(dis / len(distancelist)))
                     nxms.append(m)
         return nxms
+def combinelist(filename,con):
+    sameliters = []
+    filename = filename
+    with open(filename, 'r', encoding='utf-8') as f:  # 打开⽂件
+        lines = f.readlines()  # 读取所有⾏
+    u = lines[0].split(";;")
+    # print(u)
+    temp = []
+    for i in range(len(u)):
+        # print(type(u[i]))
+        t = u[i].replace(":", ".")
+        e = u[i].split('.')
+        m = e[0].split(":")
+        x = GDD.node_s(m[0].replace("(", ''), m[1].replace(")", ""), e[1].rstrip())
+        #print(x)
+        temp.append(x)
+    ##print(temp[5].attribute,temp[10].attribute)
+    #print(len(temp[10].attribute.rstrip()))
+    #print(DDsimilars(temp[10].attribute,'Year'))
+    list1 = []
+    liters = []
+    for i in range(len(temp)):
+        lt = []
+        if int(temp[i].name.join(filter(str.isdigit, temp[i].name))) > 0:
+            s1 = str("(" + temp[i].id + ":" + temp[i].name + ")" + '.' + temp[i].attribute + "=")
+            #print(s1)
+            l1 = str("(" + temp[i].id + ":" + temp[i].name + ")" + '.' + temp[i].attribute)
+            for q in range(0, i):
+                s2 = str("(" + temp[q].id + ":" + temp[q].name + ")" + '.' + temp[q].attribute)
+                l2 = str("(" + temp[q].id + ":" + temp[q].name + ")" + '.' + temp[q].attribute)
+                #print(temp[i].attribute,temp[q].attribute)
+                if temp[i].attribute == temp[q].attribute and re.sub('[^a-zA-Z]+', '', temp[q].name) == re.sub(
+                        '[^a-zA-Z]+', '', temp[i].name):
+                    s = s1 + s2
+                    #print(s)
+                    lt.append(l1)
+                    lt.append(l2)
+
+                    list1.append(s)
+                    liters.append(lt)
+    # print(liters)
+    file = pd.read_csv(filename, delimiter=";;", engine='python')
+    df = pd.DataFrame(file)
+    #print(len(df))
+    #print(df[liters[0][0]])
+    samenode = []
+    for liter in liters:
+        u = liter[0]
+        t = u.replace(":", ".")
+        e = u.split('.')
+        #print(e)
+        if 'id' not in e:
+            distanceset = []
+            for k in range(len(df)):
+                for p in range(len(df)):
+                    u = DDsimilars(str(df[liter[0]][k]),str(df[liter[1]][p]))
+                    distanceset.append(u)
+            tt = calculate_delta(distanceset,con)
+            print("The best delta of " + str(liter[0]+liter[1]) + " is " + str(abs(tt)))
+            pretuplelist = []
+            tuplelist = []
+            for x in range(len(df)):
+                #print(df[liter[0]][x],df[liter[1]][x],DDsimilars(df[liter[0]][x],df[liter[1]][x]),abs(tt))
+                if DDsimilars(df[liter[0]][x],df[liter[1]][x]) <= abs(tt):
+                    pretuplelist.append(x)
+            if len(pretuplelist) > 1:
+                for i in range(len(pretuplelist)):
+                    for j in range(len(pretuplelist)):
+                        if j > i:
+                            tuplelist.append((i,j))
+            m = GDD.iteml(liter[0]+' and '+liter[1], abs(tt), tuplelist, None)
+            sameliters.append(m)
+    for i in range(len(list1)):
+        samenode.append(GDD.snode(list1[i], liters[i]))
+    cosameliters = sameliters.copy()
+    for liter in sameliters:
+        if len(liter.l) == 0:
+            cosameliters.remove(liter)
+    print(cosameliters)
+    return cosameliters
 
 
 if __name__ == "__main__":
@@ -171,5 +251,6 @@ if __name__ == "__main__":
                 seg = 1
                 x = relationbolck(item,seg)
                 relation_candait.append(x)
-        print(relation_candait)
+        #print(relation_candait)
+        combinelist(filename,con)
 
