9677a4fed1cf41e64027c6deb0470ae821b56655
hujunnwei <104148715+hujunnwei@users.noreply.github.com>
2023-04-17 14:34:09 +0800

    Update and rename pre_process2.py to pre_process.py

--- New_GDDming/pre_process.py
+++ New_GDDming/pre_process.py
@@ -1,6 +1,6 @@
-index_to_delete = [2, 3]
-outfile = open('produce_Table1\'\'\'.txt', 'w', encoding='utf-8')
-with open('produce_Table1\'\'.txt', 'r', encoding='utf-8')as f:
+index_to_delete = [2, 3]  #删除索引列来限制属性个数
+outfile = open('produce_Table0\'.txt', 'w', encoding='utf-8')
+with open('produce_Table0.txt', 'r', encoding='utf-8')as f:
     for line in f:
         line = line.strip().split(";;")
         line = [line[i] for i in range(0, len(line), 1) if i not in index_to_delete]
